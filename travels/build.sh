8b7ad3ca557b1b75d2843703f67bf3bcb132f6cb
John Mazzitelli <mazz@redhat.com>
2021-03-26 09:53:09 -0400

    travels demo - move to quay.io

--- travels/build.sh
+++ travels/build.sh
@@ -5,9 +5,11 @@ DORP=${DORP:-podman}
 
 DOCKER_VERSION=v1
 
+IMAGE_HUB=quay.io
+
 ## Travel Control
 
-DOCKER_TRAVEL_CONTROL=kiali/demo_travels_control
+DOCKER_TRAVEL_CONTROL=${IMAGE_HUB}/kiali/demo_travels_control
 DOCKER_TRAVEL_CONTROL_TAG=${DOCKER_TRAVEL_CONTROL}:${DOCKER_VERSION}
 
 rm -Rf docker/travel_control/travel_control docker/travel_control/html
@@ -20,7 +22,7 @@ ${DORP} build -t ${DOCKER_TRAVEL_CONTROL_TAG} docker/travel_control
 
 ## Travel Portal
 
-DOCKER_TRAVEL_PORTAL=kiali/demo_travels_portal
+DOCKER_TRAVEL_PORTAL=${IMAGE_HUB}/kiali/demo_travels_portal
 DOCKER_TRAVEL_PORTAL_TAG=${DOCKER_TRAVEL_PORTAL}:${DOCKER_VERSION}
 
 rm -Rf docker/travel_portal/travel_portal
@@ -32,21 +34,21 @@ ${DORP} build -t ${DOCKER_TRAVEL_PORTAL_TAG} docker/travel_portal
 
 ## Travel LoadTester
 
-DOCKER_TRAVEL_LOADTESTER=kiali/demo_travels_loadtester
+DOCKER_TRAVEL_LOADTESTER=${IMAGE_HUB}/kiali/demo_travels_loadtester
 DOCKER_TRAVEL_LOADTESTER_TAG=${DOCKER_TRAVEL_LOADTESTER}:${DOCKER_VERSION}
 
 ${DORP} build -t ${DOCKER_TRAVEL_LOADTESTER_TAG} docker/travel_loadtester
 
 ## MySQL
 
-DOCKER_TRAVEL_MYSQL=kiali/demo_travels_mysqldb
+DOCKER_TRAVEL_MYSQL=${IMAGE_HUB}/kiali/demo_travels_mysqldb
 DOCKER_TRAVEL_MYSQL_TAG=${DOCKER_TRAVEL_MYSQL}:${DOCKER_VERSION}
 
 ${DORP} build -t ${DOCKER_TRAVEL_MYSQL_TAG} docker/travel_agency/mysql
 
 ## Cars
 
-DOCKER_TRAVEL_CARS=kiali/demo_travels_cars
+DOCKER_TRAVEL_CARS=${IMAGE_HUB}/kiali/demo_travels_cars
 DOCKER_TRAVEL_CARS_TAG=${DOCKER_TRAVEL_CARS}:${DOCKER_VERSION}
 
 rm -Rf docker/travel_agency/cars/cars
@@ -58,7 +60,7 @@ ${DORP} build -t ${DOCKER_TRAVEL_CARS_TAG} docker/travel_agency/cars
 
 ## Discounts
 
-DOCKER_TRAVEL_DISCOUNTS=kiali/demo_travels_discounts
+DOCKER_TRAVEL_DISCOUNTS=${IMAGE_HUB}/kiali/demo_travels_discounts
 DOCKER_TRAVEL_DISCOUNTS_TAG=${DOCKER_TRAVEL_DISCOUNTS}:${DOCKER_VERSION}
 
 rm -Rf docker/travel_agency/discounts/discounts
@@ -70,7 +72,7 @@ ${DORP} build -t ${DOCKER_TRAVEL_DISCOUNTS_TAG} docker/travel_agency/discounts
 
 ## Flights
 
-DOCKER_TRAVEL_FLIGHTS=kiali/demo_travels_flights
+DOCKER_TRAVEL_FLIGHTS=${IMAGE_HUB}/kiali/demo_travels_flights
 DOCKER_TRAVEL_FLIGHTS_TAG=${DOCKER_TRAVEL_FLIGHTS}:${DOCKER_VERSION}
 
 rm -Rf docker/travel_agency/flights/flights
@@ -82,7 +84,7 @@ ${DORP} build -t ${DOCKER_TRAVEL_FLIGHTS_TAG} docker/travel_agency/flights
 
 ## Hotels
 
-DOCKER_TRAVEL_HOTELS=kiali/demo_travels_hotels
+DOCKER_TRAVEL_HOTELS=${IMAGE_HUB}/kiali/demo_travels_hotels
 DOCKER_TRAVEL_HOTELS_TAG=${DOCKER_TRAVEL_HOTELS}:${DOCKER_VERSION}
 
 rm -Rf docker/travel_agency/hotels/hotels
@@ -94,7 +96,7 @@ ${DORP} build -t ${DOCKER_TRAVEL_HOTELS_TAG} docker/travel_agency/hotels
 
 ## Insurances
 
-DOCKER_TRAVEL_INSURANCES=kiali/demo_travels_insurances
+DOCKER_TRAVEL_INSURANCES=${IMAGE_HUB}/kiali/demo_travels_insurances
 DOCKER_TRAVEL_INSURANCES_TAG=${DOCKER_TRAVEL_INSURANCES}:${DOCKER_VERSION}
 
 rm -Rf docker/travel_agency/insurances/insurances
@@ -106,7 +108,7 @@ ${DORP} build -t ${DOCKER_TRAVEL_INSURANCES_TAG} docker/travel_agency/insurances
 
 ## Travels
 
-DOCKER_TRAVEL_TRAVELS=kiali/demo_travels_travels
+DOCKER_TRAVEL_TRAVELS=${IMAGE_HUB}/kiali/demo_travels_travels
 DOCKER_TRAVEL_TRAVELS_TAG=${DOCKER_TRAVEL_TRAVELS}:${DOCKER_VERSION}
 
 rm -Rf docker/travel_agency/travels/travels
