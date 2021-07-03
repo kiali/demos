395e614ea25c61b5d7663fa34b03647f8b7ccdea
Nick Fox <nfox@redhat.com>
2021-07-02 21:31:44 -0400

    Add bookinfo with VMs example

---
+++ bookinfo-vms/variables.tf
@@ -0,0 +1,43 @@
+variable "project_id" {
+  type        = string
+  description = "The GCP project that resources will be deployed in."
+}
+
+variable "region" {
+  type    = string
+  default = "us-central1"
+}
+
+variable "zone" {
+  type    = string
+  default = "us-central1-a"
+}
+
+variable "ssh_key" {
+  type        = string
+  description = "Your public ssh key that will be used to access the VMs remotely."
+}
+
+variable "ssh_username" {
+  type        = string
+  description = "Username to be used when connecting via ssh. This is probably your google account username e.g. just <username> from <username>@company.com"
+}
+
+# Note: trial plans will probably hit the public ip address quota in GCP when adding more than 2 nodes.
+variable "gke_num_nodes" {
+  type        = number
+  default     = 2
+  description = "number of gke nodes"
+}
+
+variable "gke_machine_type" {
+  type        = string
+  default     = "n1-standard-2"
+  description = "Machine type to use for gke nodes"
+}
+
+variable "deploy_bastion" {
+  type        = bool
+  default     = false
+  description = "If set to true, a bastion host will be deployed providing access to the bookinfo vms."
+}
