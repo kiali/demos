b4280f4314d33ba21dc9fa904d6f82f76fc1f76a
Jay Shaughnessy <jshaughn@redhat.com>
2023-07-12 16:23:39 -0400

    Add "gamma" namespace with config but no traffic.  This is primarily to help with cypress tests that need this scenario, initially to be used to improve  testing for "Show Idle Nodes" Graph Dispay option.

--- error-rates/deploy.sh
+++ error-rates/deploy.sh
@@ -3,9 +3,12 @@ set -e
 
 kubectl create namespace alpha
 kubectl create namespace beta
+kubectl create namespace gamma
 
 kubectl label namespace alpha istio-injection=enabled
 kubectl label namespace beta istio-injection=enabled
+kubectl label namespace gamma istio-injection=enabled
 
 kubectl apply -f alpha.yaml -n alpha
 kubectl apply -f beta.yaml -n beta
+kubectl apply -f beta.yaml -n gamma
--- error-rates/undeploy.sh
+++ error-rates/undeploy.sh
@@ -3,8 +3,10 @@ set -e
 
 kubectl delete -f alpha.yaml -n alpha
 kubectl delete -f beta.yaml -n beta
+kubectl delete -f gamma.yaml -n gamma
 
 kubectl delete namespace alpha
 kubectl delete namespace beta
+kubectl delete namespace gamma
 
 
